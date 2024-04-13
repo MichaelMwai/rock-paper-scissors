55fad2a247c439a9fce862e66f1ade81202760f7
Michael Mwai <michaeldmwai@gmail.com>
2024-04-13 18:10:00 +0300

    Add game function and results.)

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+const actionMessage_p = document.querySelector(".action-message");
 
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
@@ -18,9 +19,42 @@ function getComputerChoice() {
 }
 function game(userChoice) {
   const computerChoice = getComputerChoice();
+
   console.log(
     "You chose " + userChoice + " and the computer chose " + computerChoice
   );
+  switch (userChoice + " " + computerChoice) {
+    case "rock scissors":
+    case "scissors paper":
+    case "paper rock":
+      actionMessage_p.innerText =
+        "You chose " +
+        userChoice +
+        " and the computer chose " +
+        computerChoice +
+        "\n\nYOU WIN!!";
+      break;
+    case "rock paper":
+    case "paper scissors":
+    case "scissors rock":
+      actionMessage_p.innerText =
+        "You chose " +
+        userChoice +
+        " and the computer chose " +
+        computerChoice +
+        "\n\nYOU LOST!!";
+      break;
+    case "paper paper":
+    case "rock rock":
+    case "scissors scissors":
+      actionMessage_p.innerText =
+        "You chose " +
+        userChoice +
+        " and the computer chose " +
+        computerChoice +
+        "\n\nYOU TIED!!";
+      break;
+  }
 }
 
 function main() {
